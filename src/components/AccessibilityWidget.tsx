399ea899e97ef56d0e696367fe361f93afba2854
pc5888pc5888 <pc5888@gmail.com>
2026-06-09 04:21:49 +0800

    lower accessibility widget z-index

--- src/components/AccessibilityWidget.tsx
+++ src/components/AccessibilityWidget.tsx
@@ -58,7 +58,7 @@ export function AccessibilityWidget({ onChatOpen, isChatOpen }: AccessibilityWid
   }, []);
 
   return (
-    <div className="fixed bottom-6 right-6 z-[100] flex flex-col items-end gap-4 pointer-events-auto">
+    <div className="fixed bottom-6 right-6 z-[40] flex flex-col items-end gap-4 pointer-events-auto">
       <motion.div
         initial={{ opacity: 0, y: 10, scale: 0.95 }}
         animate={{
