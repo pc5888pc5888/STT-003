8c87edd16d71dfdd63b29f0b5a1ee2e7729d3a5e
pc5888pc5888 <pc5888@gmail.com>
2026-09-06 06:08:53 +0800

    design: activate approved GCSDA white-gold system

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { AppProviders } from "./app/AppProviders";
 import "./styles/tokens.css";
 import "./index.css";
 import "./styles/no-dark-theme.css";
+import "./styles/approved-gcsda.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
