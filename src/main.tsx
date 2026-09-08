95472cfc3475d920a7ff4f30c6c2bb5b4484f144
pc5888pc5888 <pc5888@gmail.com>
2026-09-09 06:07:46 +0800

    chore: load STT refinement and title rules

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import "./styles/tokens.css";
 import "./index.css";
 import "./styles/no-dark-theme.css";
 import "./styles/approved-stt.css";
+import "./styles/stt-refinement-v4.css";
+import "./titleRules";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
