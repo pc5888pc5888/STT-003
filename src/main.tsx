7d9eab0a2b364f32ab16c04244c937c7156091d8
pc5888pc5888 <pc5888@gmail.com>
2026-09-11 05:16:47 +0800

    style: activate refined STT typography and image quality tokens

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import "./styles/stt-stage2.css";
 import "./styles/stt-stage2-polish.css";
 import "./styles/stt-stage3.css";
 import "./styles/stt-native-artwork.css";
+import "./styles/stt-typography-v1.css";
 import "./titleRules";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><AppProviders><App /></AppProviders></React.StrictMode>
