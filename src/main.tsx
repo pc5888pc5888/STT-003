9d7b270abe58a800e33b9e00c3f45e61d3388886
pc5888pc5888 <pc5888@gmail.com>
2026-09-11 12:43:55 +0800

    refactor: retire conflicting STT style layers in favor of one master system

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,8 @@ import { AppProviders } from "./app/AppProviders";
 import "./styles/tokens.css";
 import "./index.css";
 import "./styles/no-dark-theme.css";
-import "./styles/approved-stt.css";
-import "./styles/stt-refinement-v4.css";
-import "./styles/stt-route-visuals-v1.css";
-import "./styles/stt-stage2.css";
-import "./styles/stt-stage2-polish.css";
-import "./styles/stt-stage3.css";
-import "./styles/stt-native-artwork.css";
-import "./styles/stt-typography-v1.css";
-import "./titleRules";
+import "./styles/stt-master-v20260911.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><AppProviders><App /></AppProviders></React.StrictMode>
 );
