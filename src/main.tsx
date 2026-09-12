263403006d011e8cf2873e42690b03ead914f170
pc5888pc5888 <pc5888@gmail.com>
2026-09-12 18:06:33 +0800

    Apply full-viewport STT layout lock

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./styles/tokens.css";
 import "./index.css";
 import "./styles/no-dark-theme.css";
 import "./styles/stt-master-v20260911.css";
+import "./styles/stt-full-viewport-lock-20260912.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><AppProviders><App /></AppProviders></React.StrictMode>
