79a1873c3b4df309a765d7c2d3736702d08e5e54
pc5888pc5888 <pc5888@gmail.com>
2026-09-15 15:45:26 +0800

    style: load final approved hero layer correction

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import "./styles/no-dark-theme.css";
 import "./styles/stt-master-v20260911.css";
 import "./styles/stt-fullpage-canonical-20260912.css";
 import "./styles/stt-approved-heroes-20260915.css";
+import "./styles/stt-hero-layer-fix-20260915.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><AppProviders><App /></AppProviders></React.StrictMode>
