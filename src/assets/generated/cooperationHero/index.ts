6df18b203221c28d37fcf970ecce5f49a9ff73a5
pc5888pc5888 <pc5888@gmail.com>
2026-09-16 11:17:31 +0800

    assets: assemble approved cooperation hero

---
+++ src/assets/generated/cooperationHero/index.ts
@@ -0,0 +1,15 @@
+import c01 from "./c01";
+import c02 from "./c02";
+import c03 from "./c03";
+import c04 from "./c04";
+import c05 from "./c05";
+import c06 from "./c06";
+import c07 from "./c07";
+import c08 from "./c08";
+import c09 from "./c09";
+import c10 from "./c10";
+import c11 from "./c11";
+import c12 from "./c12";
+
+export const COOPERATION_HERO_SRC =
+  `data:image/webp;base64,${c01}${c02}${c03}${c04}${c05}${c06}${c07}${c08}${c09}${c10}${c11}${c12}`;
