b1b0763875ece816c5f197321c046cca493a40d8
pc5888pc5888 <pc5888@gmail.com>
2026-09-21 08:11:02 +0800

    G3: load method stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import "./styles/stt-mobile-first-editorial.css";
 import "./styles/stt-g0-shell.css";
 import "./styles/stt-g1-home.css";
 import "./styles/stt-g2-problems.css";
+import "./styles/stt-g3-method.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><AppProviders><App /></AppProviders></React.StrictMode>
