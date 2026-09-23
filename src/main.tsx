922ddb9446f3c9ecddc8ed943a67fbbdbf874a42
pc5888pc5888 <pc5888@gmail.com>
2026-09-23 12:26:57 +0800

    visual: load unified title hero reference last

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import "./styles/stt-g1-home.css";
 import "./styles/stt-g2-problems.css";
 import "./styles/stt-g3-method.css";
 import "./styles/stt-g4-engagement.css";
+import "./styles/stt-title-hero-reference.css";
 import "./titleRules";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
